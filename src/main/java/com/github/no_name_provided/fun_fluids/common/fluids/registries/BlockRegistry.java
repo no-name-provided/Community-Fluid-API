13581b7e3bcb17f081b77f00f3388a537858b433
No Name Provided <>
2025-11-07 02:56:28 -0800

    Last tweaking of comments and names before I start the next content update.

--- src/main/java/com/github/no_name_provided/fun_fluids/common/fluids/registries/BlockRegistry.java
+++ src/main/java/com/github/no_name_provided/fun_fluids/common/fluids/registries/BlockRegistry.java
@@ -13,7 +13,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import static com.github.no_name_provided.fun_fluids.FunFluids.MODID;
 
 public class BlockRegistry {
-    // We use a dedicated register to avoid a bug in datagen. #Blame the NeoForged team.
+    // We use a dedicated register to avoid a bug in datagen. #BlameTheNeoForgedTeam.
     public static final DeferredRegister.Blocks FLUID_BLOCKS = DeferredRegister.createBlocks(MODID);
 
     public static final DeferredHolder<Block, LiquidBlock> COOL_LAVA_BLOCK = FLUID_BLOCKS.register(
@@ -71,7 +71,7 @@ public class BlockRegistry {
     @SuppressWarnings("deprecation")
     public static final DeferredHolder<Block, AbstractCauldronBlock> COOL_LAVA_CAULDRON = SOLID_BLOCKS.register(
         "cool_lava_cauldron_block",
-            () -> new CoolLavaCauldronBlock(BlockBehaviour.Properties.ofLegacyCopy(Blocks.CAULDRON).lightLevel(p_152690_ -> 15))
+            () -> new CoolLavaCauldronBlock(BlockBehaviour.Properties.ofLegacyCopy(Blocks.CAULDRON).lightLevel(state -> 15))
     );
 
     public static void register(IEventBus bus) {
