efb23e2edc1386b3a4b33c68ccdd8400bf7f197a
No Name Provided <>
2026-06-12 21:02:19 -0700

    Simplified Minecraft mixin. Still not sure what happened to the background music.
    May have identified a bug where ambient noise doesn't tick fast enough to stop while transitioning between two adjacent fluids, and multiple instances can end up playing simultaneously (because the old instances don't spin down).

--- src/main/java/com/github/no_name_provided/cfa/mixin/CFA_Minecraft.java
+++ src/main/java/com/github/no_name_provided/cfa/mixin/CFA_Minecraft.java
@@ -1,12 +1,10 @@
 package com.github.no_name_provided.cfa.mixin;
 
-import com.github.no_name_provided.cfa.mixin_interfaces.IFluidTypeExtension;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.blaze3d.platform.WindowEventHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.util.thread.ReentrantBlockableEventLoop;
-import net.neoforged.neoforge.fluids.FluidType;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,12 +31,6 @@ abstract class CFA_Minecraft extends ReentrantBlockableEventLoop<Runnable> imple
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isUnderWater()Z"))
     private boolean cfa_getSituationalMusic_isUnderWater(boolean original) {
         // The player is null-checked before we reach this point, so we can assume it isn't null
-        FluidType lastFluid = Objects.requireNonNull(player).getLastFluid();
-        return original ||
-                (
-                        (((IFluidTypeExtension) lastFluid).hasUnderWaterMusic() &&
-                                // This is a simplified version of the vanilla check, which seems unnecessarily nuanced. Behavior will differ slightly
-                                    player.fluidInteraction.isEyeInFluid(((IFluidTypeExtension) lastFluid).getTag()))
-                );
+        return original || Objects.requireNonNull(player).getWasUnderLastFluid();
     }
 }
